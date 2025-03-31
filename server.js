602945e631a9608c266df487da533c22f2f3bc52
tigerlab <tiggers12@hotmail.com>
2025-03-31 23:15:38 +0700

    แก้ controller

--- server.js
+++ server.js
@@ -14,12 +14,30 @@ app.use(express.urlencoded({ extended: true }));
 // controllers
 //
 const { UserController } = require("./controllers/UserController");
+const { CompanyController } = require("./controllers/CompanyController");
+const { ProductController } = require("./controllers/ProductController");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//
+//user
+//
 app.post("/api/user/signin", UserController.signIn);
+
+//
+//company
+//
+app.post("/api/company/create",CompanyController.create);
+app.get("/api/company/list", CompanyController.list);
+//
+//buy
+//
+app.post("/api/buy/create",ProductController.create);
+app.get("/api/buy/list",ProductController.list);
+app.put("/api/buy/update/:id",ProductController.update);
+app.delete("/api/buy/remove/:id",ProductController.remove);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
